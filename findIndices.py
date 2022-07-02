7b4c07e988f130de93abc8a464ea2f7da168504c
Grant Grant <48664405+grandaphi@users.noreply.github.com>
2022-07-02 08:20:34 +0000

    started on finding indices challenge, just notes so far

---
+++ findIndices.py
@@ -0,0 +1,4 @@
+#  find indices of matched objects in list, including lists of any depth
+#  must take two arguments; 1. list to search, and 2. value to match
+#  must have one output; 1. list of indices
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 prog = input("""
 For Integers enter 1.
 For palindrome enter 2.
-For Sorting Strings enter 3.
+For sorting strings enter 3.
+For finding indices, enter 4.
 """)
 
 if prog == "1":
@@ -10,6 +11,8 @@ elif prog == "2":
     import palindrome
 elif prog == "3":
     import sortString
+elif prog== "4":
+    import findIndices
 else:
     print("""
 No valid option selected, exiting.
