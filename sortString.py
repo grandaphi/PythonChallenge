805d717781ce6c4051df737744c4093ca1f89d90
Grant Grant <48664405+grandaphi@users.noreply.github.com>
2022-07-02 07:18:49 +0000

    working as per spec, case insensitive sorting of words in alphabetic order returned as a string with original case

--- sortString.py
+++ sortString.py
@@ -1,9 +1,18 @@
-# accept input string and sort words alaphabetically and return them, with case as was input
+#  accept input string and sort words alaphabetically and return them, with case as was input
+#  solution found in W3schools refernce string.split () method and list.sort() method with key=str.lower to ignore case
+#  .join method to return list as string , as per spec in challenge
 
-rawString = input("Enter a string to have the words sorted alphabetically:\n\r")
-rawString = rawString.split()
-print(rawString)
+def SortString():
+    rawString = input("Enter a string to have the words sorted alphabetically:\n\r")
 
-rawString.sort()
+    splitList = rawString.split()
 
-print(rawString)
+    splitList.sort(key = str.lower)
+
+    jointString = ' '.join(splitList)
+
+    print(jointString)
+
+    return
+
+SortString()
