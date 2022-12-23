44bfc0f2ebf9c9bb6442807439173c773fad1bc5
Grant Grant <48664405+grandaphi@users.noreply.github.com>
2022-12-23 11:30:50 +0000

    [working] word counts are working

--- words.py
+++ words.py
@@ -1,24 +1,32 @@
 #  find unique words in file and list top ten most common words
-test = "one two three one"
-with open("pg100.txt",'r') as file:
-    lines = [line.strip().strip('.').strip(';').strip(':').split() for line in file]
-#  append if no match
-#  increment count if match
-# print(lines)
+import re, heapq
 
-#  TODO:  work better with alphanumerics and also ignore case
+nMaximaWords = 20
+max = 0
+topword=[]
 wordict ={}
+totalWords = 0
+
+
+with open("pg100.txt",'r') as file:
+    lines = [line.split() for line in file]
 
 for i in range(len(lines)):
     for j in range(len(lines[i])):
-        if not(lines[i][j] in wordict):
-            wordict[lines[i][j]]=1
+        word = (re.sub("[^0-9a-zA-Z-']",'',lines[i][j])).lower()
+        if not(word in wordict):
+            wordict[word]=1
         else:
-            wordict[lines[i][j]]+=1
+            wordict[word]+=1
+        totalWords+=len(lines[i])
 
-print(wordict)
+topN = heapq.nlargest(nMaximaWords , wordict, key=wordict.get)
 
-#    for j in test.split():
-#        if test.split()[i] == test.split()[i+j]:
-#            print("match")
 
+for i, j in wordict.items():
+    if i in topN:
+        topword.append([i,j])
+        max = j
+
+print(topword)
+print("Total words: ", totalWords)
