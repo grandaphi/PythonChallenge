be4c9c268667526ecc68435f0ab65cc250ff6364
Grant Grant <48664405+grandaphi@users.noreply.github.com>
2022-12-29 12:37:37 +0000

    [working] generatePW works

--- generatePW.py
+++ generatePW.py
@@ -2,16 +2,21 @@
 # output : random list of n words
 from random import randint as r
 diceDict = {}
-diceroll = ""
 
 with open("dicelist.txt", 'r') as file:
     for line in file:
         key, value = line.strip().split()
         diceDict[key] = value
 
-for i in range(5):
-    diceroll+=str(r(1,6))
-print(diceroll)
-
-print(diceDict[diceroll])                  
-#  TODO: generate 5 digit random number for each of n words and return them from the dictionary
+def genWords(numWords):
+    words = []
+    for n in range(numWords):
+        diceroll = ""
+        for i in range(5):
+            diceroll+=str(r(1,6))
+        print(diceroll)
+        words.append(diceDict[diceroll])
+    print(words)
+    return words
+    
+        
