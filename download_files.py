3eb442ebcbbf114a42be570b7c5f516e231e38a6
Grant Grant <48664405+grandaphi@users.noreply.github.com>
2023-03-18 02:43:45 +0000

    [WORKING] download sequential files working. All challenges completed to satisfactory level. Needs clean up if being shared or displayed.

--- download_files.py
+++ download_files.py
@@ -5,11 +5,49 @@
 
 #  sample URL: https://699340.youcanlearnit.net/image001.jpg
 
-import requests
+import requests, re, os
 
-url = 'https://699340.youcanlearnit.net/image001.jpg' 
+newUrl = url = 'https://699340.youcanlearnit.net/image001.jpg'
+folder = './DL_files/'
+inc = '001'
 
-r = requests.get(url, allow_redirects=True)
-with open('./DL_files/image001.jpg', 'wb+') as f:
-    f.write(r.content)
 
+def url_ok(url):
+    try:
+        response = requests.head(url)
+
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.ConnectionError as e:
+        return e
+
+
+while url_ok(newUrl):
+    r = requests.get(newUrl, allow_redirects=True)
+
+    #   use OS to easily get file extension
+    filename, file_extension = os.path.splitext(newUrl)
+    lastinc = inc
+
+    #  assuming last match is correct one, allow to run through matches and use last one only
+    for match in re.finditer("[0-9]+", newUrl):
+
+        inc = (newUrl[match.start():match.end()])
+
+    #  update image path to save to 
+    imagepath = folder + 'image' + inc + file_extension
+
+    with open(imagepath, 'wb+') as f:
+        f.write(r.content)
+
+    # increment sequence number in string
+    inc = str(int(inc) + 1).zfill(len(inc))
+
+    #  update URL with next number in sequence
+    newUrl = newUrl.replace(lastinc,inc)
+    
+
+
+print("no response from:", newUrl)
